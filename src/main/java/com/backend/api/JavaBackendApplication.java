94d3fa8d0ef7c28e8debd30e214ccbe712f3ac6d
looh16 <langacustodio@gmail.com>
2018-03-22 21:24:31 +0200

    criando operacao de instanciacao

--- src/main/java/com/backend/api/JavaBackendApplication.java
+++ src/main/java/com/backend/api/JavaBackendApplication.java
@@ -1,12 +1,37 @@
 package com.backend.api;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.assertj.core.util.Arrays;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class JavaBackendApplication {
+import com.backend.api.models.Categoria;
+import com.backend.api.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class JavaBackendApplication implements CommandLineRunner{
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(JavaBackendApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+
+		Categoria cat1 = new Categoria(null, "informatica");
+		Categoria cat2 = new Categoria(null, "Fisica");
+		
+		List<Categoria> todas = new ArrayList<>();
+		todas.add(cat1);
+		todas.add(cat2);
+		
+		categoriaRepository.saveAll(todas);
 	}
 }
