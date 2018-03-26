ab03e48806e84816663ebe639a77dd9b391cb745
looh16 <langacustodio@gmail.com>
2018-03-26 20:49:29 +0200

    endepoint /clientes/{id} disponivel

---
+++ src/main/java/com/backend/api/services/ClienteService.java
@@ -0,0 +1,25 @@
+package com.backend.api.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.backend.api.models.Cliente;
+import com.backend.api.repositories.ClienteRepository;
+import com.backend.api.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class ClienteService {
+	
+	@Autowired
+	private ClienteRepository repo;
+	
+	public Cliente buscar(Integer id) {
+		
+		Optional<Cliente> obj = repo.findById(id);
+		
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objecto nao encontrado! id " + id + ", tipo:" + Cliente.class.getName()));
+	}
+
+}
